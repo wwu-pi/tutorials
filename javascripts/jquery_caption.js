5eec5251f6d7a99a937d6e7208a423620dea6685
cusen_01 <cusen_01@uni-muenster.de>
2013-07-02 18:15:58 +0200

    Fixed Bug in jquery_caption.js: Problems with formated caption.

--- javascripts/jquery_caption.js
+++ javascripts/jquery_caption.js
@@ -2,10 +2,11 @@ $(document).ready(function() {
 	// Every image referenced from a Markdown document
 	$(".recipe-content img").parent().each(function() {
 		// Let's put a caption if there is one
-		if($(this).contents().first().is('img') && $(this).contents().size() == 2 ) {
-           $(this).contents().eq(0).wrap('<div class="image"></div>');
-           $(this).contents().eq(1).wrap('<div class="caption"></div>');
-           $(this).replaceWith($('<div class="figure">' + this.innerHTML + '</div>'));
+		if($(this).contents().first().is('img')) {
+           var img = $(this).contents().eq(0).detach();
+           $(this).replaceWith($('<div class="figure"><div class="image">'+ 
+            img[0].outerHTML + '</div><div class="caption">' + 
+            this.innerHTML + '</div></div>'));
           }
 		});
 });
