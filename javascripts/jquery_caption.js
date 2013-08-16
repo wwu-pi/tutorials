b700057b89d7d4223f38048af84c5f26b2866de9
Jan Ernsting <jan.ernsting@gmail.com>
2013-08-16 13:31:42 +0200

    Unify indendation use spaces

--- javascripts/jquery_caption.js
+++ javascripts/jquery_caption.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
-	// Every image referenced from a Markdown document
-	$(".recipe-content img").parent().each(function() {
-		// If the first element of an within an element is an img tag...
-		if($(this).contents().first().is('img')) {
-          // take the images html
-          var imgHtml = $(this).contents().first().detach()[0].outerHTML;
-          var captionHtml = this.innerHTML;
-          // skip captioning in case of no available caption
-          if (captionHtml.trim.length == 0)
-            return;
-          $(this).replaceWith(
-              '<div class="figure">' +
-                '<div class="image">' +
-                  imgHtml +
-                '</div>' +
-                '<div class="caption">' +
-                  captionHtml +
-                '</div>' +
-              '</div>'
-            );
-          }
-		});
+  // Every image referenced from a Markdown document
+  $(".recipe-content img").parent().each(function() {
+    // If the first element of an within an element is an img tag...
+    if($(this).contents().first().is('img')) {
+      // take the images html
+      var imgHtml = $(this).contents().first().detach()[0].outerHTML;
+      var captionHtml = this.innerHTML;
+      // skip captioning in case of no available caption
+      if (captionHtml.trim.length == 0)
+        return;
+      $(this).replaceWith(
+        '<div class="figure">' +
+          '<div class="image">' +
+            imgHtml +
+          '</div>' +
+          '<div class="caption">' +
+            captionHtml +
+          '</div>' +
+        '</div>'
+      );
+    }
+  });
 });
