d974fbaf34718f7e3fe249aa0dfeef0b00b1a89f
Jan Ernsting <jan.ernsting@gmail.com>
2013-08-23 13:47:15 +0200

    Ensure captions are only shown when existent

--- javascripts/jquery_caption.js
+++ javascripts/jquery_caption.js
@@ -3,13 +3,21 @@ $(document).ready(function() {
   $(".recipe-content img").parent().each(function() {
     // If the first element of an within an element is an img tag...
     if($(this).contents().first().is('img')) {
-      // take the images html
-      var imgHtml = $(this).contents().first().detach()[0].outerHTML;
+      // detach the image's html from the DOM
+      var detachedImgHtml = $(this).contents().first().detach()[0].outerHTML;
       var captionHtml = this.innerHTML;
+
+      // check if caption exists
+      if ($.trim(captionHtml).length == 0) {
+        // replace the current with the previously detached imgHtml
+        $(this).replaceWith(detachedImgHtml);
+        return;
+      }
+
       $(this).replaceWith(
         '<div class="figure">' +
           '<div class="image">' +
-            imgHtml +
+            detachedImgHtml +
           '</div>' +
           '<div class="caption">' +
             captionHtml +
